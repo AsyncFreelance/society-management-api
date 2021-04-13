528ca2d9e9b58d811723a422b3ffba621fcecc7c
async69 <yeabsera0830@gmail.com>
2021-04-13 12:41:20 +0300

    Fixed bugs

--- src/auth/resolvers/Wallet/functions/fetchWalletByUserID.ts
+++
@@ -1,8 +0,0 @@
-import { WalletModel } from "../../../models/Wallet/wallet.model"
-
-const fetchWalletByUserID = async (userID: string) => {
-    const wallet = await WalletModel.findOne({ userID })
-    return wallet
-}
-
-export default fetchWalletByUserID
--- src/auth/resolvers/Wallet/functions/fetchWallets.ts
+++
@@ -1,8 +0,0 @@
-import { WalletModel } from "../../../models/Wallet/wallet.model"
-
-const fetchWallets = async () => {
-    const wallets = await WalletModel.find({})
-    return wallets
-}
-
-export default fetchWallets
